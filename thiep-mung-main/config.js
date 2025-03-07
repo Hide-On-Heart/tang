f75c478220b0f31990b35727ee8e316ecd1b6bf3
HideOnHeart10 <nakzed132@gmail.com>
2025-03-07 22:07:01 +0700

    First commit

---
+++ thiep-mung-main/config.js
@@ -0,0 +1,8 @@
+const nameGirl = 'Em gái!';
+const giftUrl = '';
+const eventName = 'Chúc Mừng 8-3';
+const titleCard = 'Tặng Em gái';
+const contentCard = 'Chúc em 8/3 tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với em trong hôm nay và cả những ngày sau';
+const giftImage = './z6384719109134_7cb2de57b7dac785f3291c3eb824e605.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
